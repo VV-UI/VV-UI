83641e8647fa743ccb49ab02ddb5d1742dfd35fd
monkeyWangs <2424880409@qq.com>
2017-11-11 12:55:53 +0800

    update webpack files

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -79,7 +79,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('examples'), resolve('test')]
+        include: [resolve('examples'), resolve('test'), resolve('packages')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
