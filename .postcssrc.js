c9c1e488203dea790687d3fc86788e9942754f23
autumnLeaves0 <461563994@qq.com>
2017-11-17 10:09:45 +0800

    feat: update alert component css

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,20 +3,6 @@
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
-    "postcss-salad":{
-      "features": {
-        "bem": {
-          "shortcuts": {
-            "component": "b",
-            "modifier": "m",
-            "descendent": "e"
-          },
-          "separators": {
-            "descendent": "__",
-            "modifier": "--"
-          }
-        }
-      }
-    }
+    "autoprefixer":{}
   }
 }
