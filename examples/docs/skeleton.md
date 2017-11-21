225049e3c8b9ba54fa74092790170a22b48cb358
monkeyWangs <2424880409@qq.com>
2017-11-21 19:41:15 +0800

    feat: 骨架屏幕

---
+++ examples/docs/skeleton.md
@@ -0,0 +1,8 @@
+# skeleton 骨架屏
+----
+### 基础用法
+文本超出显示长度，折叠起来，通过len属性显示从何处开始折叠。
+
+<div class="demo-block">
+  <w-skeleton></w-skeleton>
+</div>
---
+++ packages/skeleton/src/skeleton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w__skeleton">
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'WSkeleton',
+    props: {
+      proportion: '1' // w : h
+    },
+    data () {
+      return {}
+    }
+  }
+</script>
